992bed33ed921616ce3c50e2a87e14c6776f24bf
Mike Field-May <mfieldmay@gmail.com>
2017-10-02 14:43:22 +0100

    Add keyframe animations to list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from "@angular/animations";
+import { trigger, state, style, transition, animate, keyframes } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -14,19 +14,108 @@ import { trigger, state, style, transition, animate } from "@angular/animations"
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
       })),
-      transition('normal => highlighted', animate(300)), // transition takes a string which indicates whichway the animation should go.
-      transition('highlighted => normal', animate(600)),// animate is a method. At it's most basic if you input a number it takes that mount of time to transition
+      transition('normal => highlighted', animate(300)), // transition takes a string which indicates whichway the animation should go. For back and forth use the <=> arrow.
+      transition('highlighted => normal', animate(600)), // animate is a method. At it's most basic if you input a number it takes that mount of time to transition
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)',
+        borderRadius: '0px'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)',
+        borderRadius: '0px'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)',
+        borderRadius: '0px'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ]) // if the element didn't have a state to start with you should use the void state.
+      // when using the void state we need to declare a style that the item has before the state is triggered
+    ]),
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          })
+        ]))
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
     ])
-  ] // we specify the animations this component should be aware of
+  ]
 })
 
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
   state = 'normal'; // angular animation transfers from one state to another
+  wildState = 'normal';
 
 	onAdd(item) {
 		this.list.push(item);
-	}
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
 
 	onDelete(item) {
 		this.list.splice(this.list.indexOf(item), 1);
@@ -34,5 +123,7 @@ export class AppComponent {
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+
   }
 }

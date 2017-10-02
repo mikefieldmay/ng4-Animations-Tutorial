360be5025c4cb01ab26f478555e2b9be0e402722
Mike Field-May <mfieldmay@gmail.com>
2017-10-02 11:48:29 +0100

    Basic animations setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { trigger, state, style } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ] // we specify the animations this component should be aware of
 })
+
 export class AppComponent {
-	list = ['Milk', 'Sugar', 'Bread'];
+  list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal'; // angular animation transfers from one state to another
 
 	onAdd(item) {
 		this.list.push(item);
